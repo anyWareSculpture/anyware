259d5ae3d47bc2d74b4f81ab9fd5ba1de6d01666
Sunjay Varma <varma.sunjay@gmail.com>
2015-06-01 00:40:09 -0400

    Added submit coverage task

---
+++ tasks/submit-coverage-task.js
@@ -0,0 +1,16 @@
+var codecov = require('gulp-codecov.io');
+
+/**
+ * Submits code coverage to codecov.io
+ * This task will only work from a Travis CI like environment
+ * @param gulp - the instance of the gulp module to use
+ * @param {String} taskName - The name of the task to create
+ * @param {String} [coverageFile='coverage/lcov.info'] - The path to the coverage file to submit
+ */
+module.exports = function(gulp, taskName, coverageFile) {
+  coverageFile = coverageFile || 'coverage/lcov.info';
+  gulp.task(taskName, function() {
+    return gulp.src(coverageFile)
+      .pipe(codecov());
+  });
+};
