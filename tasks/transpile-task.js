e05d37a71816a4d5f1a51ea32f9d9928604f7a15
Sunjay Varma <varma.sunjay@gmail.com>
2015-06-19 00:19:57 -0400

    Enabled stage 0 experimental features in babel

--- tasks/transpile-task.js
+++ tasks/transpile-task.js
@@ -10,7 +10,10 @@ module.exports = function(gulp, taskName, targetFiles, destinationDirectory) {
   gulp.task(taskName, function() {
     return gulp.src(targetFiles)
       .pipe(sourcemaps.init())
-      .pipe(babel())
+      .pipe(babel({
+        // enables stage 0 experimental features
+        stage: 0
+      }))
       .pipe(sourcemaps.write())
       .pipe(gulp.dest(destinationDirectory));
   });
