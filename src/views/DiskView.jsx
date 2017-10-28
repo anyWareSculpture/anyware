6f9ce36a8e41602ecf94a120046d5239c614e012
Marius Kintel <marius@kintel.net>
2017-10-28 17:20:07 -0400

    Support aesthetic disks with opposite rotation, handle stroke-width, correctly handle color overrides

--- src/views/DiskView.jsx
+++ src/views/DiskView.jsx
@@ -3,21 +3,33 @@ import SculptureStore from '../game-logic/sculpture-store';
 import DiskGameLogic from '../game-logic/logic/disk-game-logic';
 import Graphics from './svg/disk-game.svg';
 
-const diskOrigins = {
+const diskConfig = {
   level0: {
-    disk0: [374.66, 570.39],
-    disk1: [219.369, 322.276],
-    disk2: [439.437, 161.5],
+    stroke: "none",
+    disk0: [513.32, 444.145],
+    disk1: [186.945, 444.14],
+    disk2: [350.33, 161.9],
+    disk0Aesthetic: [513.32, 444.145],
+    disk1Aesthetic: [186.945, 444.14],
+    disk2Aesthetic: [350.33, 161.9],
   },
   level1: {
-    disk0: [350, 350],
-    disk1: [350, 350],
-    disk2: [350, 350],
+    stroke: "none",
+    disk0: [513.32, 444.145],
+    disk1: [186.945, 444.14],
+    disk2: [350.33, 161.9],
+    disk0Aesthetic: [513.32, 444.145],
+    disk1Aesthetic: [186.945, 444.14],
+    disk2Aesthetic: [350.33, 161.9],
   },
   level2: {
-    disk0: [350, 350],
-    disk1: [468.5, 426.5],
-    disk2: [192, 213],
+    stroke: "#000000",
+    disk0: [453.1, 409.205],
+    disk1: [245.366, 410.247],
+    disk2: [350,230.165],
+    disk0Aesthetic: [143.104,231.203],
+    disk1Aesthetic: [569.5, 232.184],
+    disk2Aesthetic: [350, 587.797],
   },
 };
 
@@ -55,6 +67,9 @@ export default class DiskView extends React.Component {
       disk0Color: '#000000',
       disk1Color: '#000000',
       disk2Color: '#000000',
+      disk0Stroke: 1,
+      disk1Stroke: 1,
+      disk2Stroke: 1,
       level: 0,
       active: false,
       showCircle: false,
@@ -89,24 +104,33 @@ export default class DiskView extends React.Component {
     if (changes.lights && changes.lights[this.props.config.LIGHTS.ART_LIGHTS_STRIP]) {
       this.setState({showCircle: this.lightArray.getIntensity(this.props.config.LIGHTS.ART_LIGHTS_STRIP, '3') > 0});
     }
-    if (changes.disk && changes.disk.state) {
+    if (changes.disk) {
       // Show puzzle when active
-      this.setState({
-        showPuzzle: [DiskGameLogic.STATE_FADE_IN, DiskGameLogic.STATE_SHUFFLE, DiskGameLogic.STATE_ACTIVE, DiskGameLogic.STATE_WINNING].includes(changes.disk.state)
-      });
+      if (changes.disk.state) {
+        this.setState({
+          showPuzzle: [DiskGameLogic.STATE_FADE_IN, DiskGameLogic.STATE_SHUFFLE, DiskGameLogic.STATE_ACTIVE, DiskGameLogic.STATE_WINNING].includes(changes.disk.state)
+        });
+      }
       // Handle level changes
       if (changes.disk.hasOwnProperty('level')) {
         this.setState({level: changes.disk.level});
       }
       // Show location color of moving disks
+      // FIXME: Solve this in a better way:
+      if (this.state.level >= 3) return;
       for (const diskId of this.disks) {
+        let color = diskConfig[`level${this.state.level}`].stroke;
+        let stroke = 1;
         const userId = this.disks.get(diskId).getUser();
         if (userId) {
           const colorId = this.props.config.getLocationColor(userId);
           if (colorId) {
-            this.setState({ [`${diskId}Color`]: this.props.config.getWebColor(colorId) });
+            color = this.props.config.getWebColor(colorId);
+            stroke = 5;
           }
         }
+        this.setState({ [`${diskId}Color`]: color,
+                        [`${diskId}Stroke`]: stroke });
       }
     }
   }
@@ -138,11 +162,22 @@ export default class DiskView extends React.Component {
         return <use key={diskId}
                     xlinkHref={`#level${this.state.level}-${diskId}`}
                     style={{
-                      transformOrigin: diskOrigins[`level${this.state.level}`][diskId].map((c) => `${c}px`).join(' '),
+                      transformOrigin: diskConfig[`level${this.state.level}`][diskId].map((c) => `${c}px`).join(' '),
                       transform: `rotate(${this.state[diskId]}deg)`,
                       opacity: this.state.showPuzzle ? 1 : 0,
                       transition: "opacity 2s ease-in",
                       stroke: this.state[`${diskId}Color`],
+                      strokeWidth: this.state[`${diskId}Stroke`],
+                    }}/>;
+      }) }
+      { ['disk0', 'disk1', 'disk2'].map((diskId) => {
+        return <use key={diskId}
+                    xlinkHref={`#level${this.state.level}-${diskId}-aesthetic`}
+                    style={{
+                      transformOrigin: diskConfig[`level${this.state.level}`][`${diskId}Aesthetic`].map((c) => `${c}px`).join(' '),
+                      transform: `rotate(${-this.state[diskId]}deg)`,
+                      opacity: this.state.showPuzzle ? 1 : 0,
+                      transition: "opacity 2s ease-in",
                     }}/>;
       }) }
       </g>
